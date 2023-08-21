5a66363238b582285707f2c68a4531e068af1dfa
Garrett Simpson <dotunwrap@pm.me>
2023-08-21 08:47:28 -0400

    Keeping active style on Home

--- src/lib/components/NavButton.svelte
+++ src/lib/components/NavButton.svelte
@@ -5,7 +5,7 @@
     export let href: string;
 </script>
 
-<a href="{href}" class:active={$page.url.pathname === href} class="rounded-full p-2 m-1 text-subtext1 hover:bg-crust transition-colors">
+<a href="{href}" class:active={$page.url.pathname === href || $page.url.pathname === href.slice(0, -1)} class="rounded-full p-2 m-1 text-subtext1 hover:bg-crust transition-colors">
     {text}
 </a>
 
